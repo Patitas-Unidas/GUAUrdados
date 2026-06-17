6ba0c17f687e5fcd27cad27ce8c4b48459bc6658
alezamorav <alezamorav@usm.cl>
2026-06-17 13:32:46 -0400

    Arreglo en fuente y comienzo de homepage

---
+++ lib/homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListaTodosLosPerros extends StatelessWidget {
+  const ListaTodosLosPerros({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos los perritos'),
+      ),
+    );
+  }
+}
