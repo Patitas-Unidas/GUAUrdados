b9ecf9f98a010d78168ac3e0ee08b75245c35f1b
alezamorav <alezamorav@usm.cl>
2026-09-03 11:03:28 -0400

    arreglé orden de componentes

--- lib/screens/foro.dart
+++ lib/screens/foro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:guaurdados_oficial/screens/nuevapublicacion.dart';
+import 'nuevapublicacion.dart';
+import 'componentesforo.dart';
 
 class Foro extends StatelessWidget {
   const Foro({
@@ -195,18 +196,22 @@ class Foro extends StatelessWidget {
 
               // Publicaciones
               Publicacion(
+                TipoPub: 'General',
                 TextoPub: 'Le di su desayuno hoy a las 7am, se lo comió todo muy rápido! Le noté la cola moviéndose sin parar :)',
+                IDPerro: perros[int.parse(IDPerro!)][1],
               ),
 
               if (IDPerro != null) ...[
-                ActualizacionComida(
+                Publicacion(
+                  TipoPub: 'Alimentación',
                   IDPerro: perros[int.parse(IDPerro!)][1],
                   users: users,
                   comida: ['500 gr', 'croquetas'],
                   tiempo: ['hoy', '12:43'],
                 ),
 
-                ActualizacionEstadoMedico(
+                Publicacion(
+                  TipoPub: 'Estado Médico',
                   IDPerro: perros[int.parse(IDPerro!)][1], 
                   users: users, 
                   procedimiento: 'desparasitación',
@@ -349,7 +354,8 @@ class Foro extends StatelessWidget {
               ),
 
               if (IDPerro != null) ...[
-                ActualizacionComida(
+                Publicacion(
+                  TipoPub: 'Alimentación',
                   IDPerro: perros[int.parse(IDPerro!)][1],
                   users: users,
                   comida: ['500 gr', 'croquetas'],
@@ -425,319 +431,6 @@ class Foro extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class ActualizacionComida extends StatelessWidget {
-  const ActualizacionComida({
-    super.key,
-    required this.IDPerro,
-    required this.users,
-    required this.comida,
-    required this.tiempo,
-  });
-  final String IDPerro;
-  final List<String> users;
-  final List<String> comida;
-  final List<String> tiempo;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      color: Color.fromRGBO(222, 79, 65, 1),
-      child: Padding(
-        padding: EdgeInsets.only(left: 5, top: 10, bottom: 10, right: 5),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.flatware,
-              color: Colors.white,
-              size: 40,
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '¡${users[0]} a registrado una nueva alimentación!',
-                    overflow: TextOverflow.fade,
-                    maxLines: 1,
-                    softWrap: false,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ), 
-                  Padding(
-                    padding: EdgeInsets.all(1),
-                    child:  Expanded( 
-                      child: Text(
-                        'Se ha registrado que $IDPerro ha recibido ${comida[0]} de ${comida[1]} ${tiempo[0]} a las ${tiempo[1]}.',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ActualizacionEstadoMedico extends StatelessWidget {
-  const ActualizacionEstadoMedico({
-    super.key,
-    required this.IDPerro,
-    required this.users,
-    required this.procedimiento,
-    required this.tiempo,
-  });
-  final String IDPerro;
-  final List<String> users;
-  final String procedimiento;
-  final List<String> tiempo;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      color: Color.fromRGBO(31, 179, 156, 1),
-      child: Padding(
-        padding: EdgeInsets.only(left: 5, top: 10, bottom: 10, right: 5),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.medical_information_outlined,
-              color: Colors.white,
-              size: 40,
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '¡${users[2]} ha actualizado el estado médico!',
-                    overflow: TextOverflow.fade,
-                    maxLines: 1,
-                    softWrap: false,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ), 
-                  Padding(
-                    padding: EdgeInsets.all(1),
-                    child:  Expanded( 
-                      child: Text(
-                        'Se ha registrado que $IDPerro ha recibido el siguiente procedimiento ${tiempo[0]} a las ${tiempo[1]}: $procedimiento.',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Publicacion extends StatelessWidget {
-  const Publicacion({
-    super.key,
-    required this.TextoPub,
-    this.ImagenPub,
-  });
-  final String TextoPub;
-  final String? ImagenPub;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          BarraUsuario(
-            IDUsuario: 'hola',
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 15, right: 15, bottom: 20,),
-            child: Flexible(
-              child: Text(
-                TextoPub,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-            ),
-          ),
-          Divider(
-            thickness: 1.1,
-          ),
-          BarraAcciones(
-            IDPublicacion: 'hola',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BarraUsuario extends StatelessWidget {
-  const BarraUsuario({
-    super.key,
-    required this.IDUsuario,
-  });
-  final String IDUsuario;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              CircleAvatar(),
-              Padding(
-                padding: EdgeInsets.only(left: 10,),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'María González',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      '@mariag   Hace 2 horas',
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Color.fromRGBO(56, 54, 53, .5),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: InkWell(
-              onTap: () {},
-              child: Icon(Icons.more_horiz),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BarraAcciones extends StatelessWidget {
-  const BarraAcciones({
-    super.key,
-    required this.IDPublicacion,
-  });
-  final String IDPublicacion;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 5, left: 25, right: 25, bottom: 10,),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 20),
-                child: InkWell(
-                  onTap: () {},
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 3),
-                        child: Icon(
-                          Icons.favorite_border,
-                          color: Color.fromRGBO(56, 54, 53, .5),
-                          size: 20,
-                        ),
-                      ),
-                      Text(
-                        '14',
-                        style: TextStyle(
-                          color: Color.fromRGBO(56, 54, 53, .5),
-                          fontSize: 15,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-              InkWell(
-                onTap: () {},
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 5),
-                      child: Icon(
-                        Icons.chat_bubble_outline,
-                        color: Color.fromRGBO(56, 54, 53, .5),
-                        size: 19,
-                      ),
-                    ),
-                    Text(
-                      '3',
-                      style: TextStyle(
-                        color: Color.fromRGBO(56, 54, 53, .5),
-                        fontSize: 15,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-              
-            ],
-          ),
-
-          InkWell(
-            onTap: () {},
-            child: Icon(
-              Icons.emoji_emotions_outlined,
-              color: Color.fromRGBO(56, 54, 53, .5),
-              size: 20,
-            ),
-          ),
-        ],
       ),
     );
   }
